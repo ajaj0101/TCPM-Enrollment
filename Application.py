e41ee490e75e45910ff25cfc1faf2fa04f13fa9e
ajaj0101 <achiang1111@gmail.com>
2022-06-17 20:46:21 -0400

    finished emergency contact class. moving onto the short answer bottom questions

--- Application.py
+++ Application.py
@@ -11,21 +11,37 @@ class Enrollee:
         self._occ = occ
         self._workPhone = workPhone
 
-    def displayEnrollee(self):
+    def display_Enrollee(self):
         data = [["NAME: "+self._name, "BIRTHDAY: "+self._bday, "SEX: "+self._sex],
                 ["ADDRESS: "+self._addr, "HOME PHONE: "+self._homePhone], 
                 ["CITY: "+self._city, "STATE: "+self._state, "ZIP: "+self._zip],
                 ["OCCUPATION: "+self._occ, "WORK PHONE: "+self._workPhone]]
+        print("-----------------------------------------------------------")
         col_width = max(len(word) for row in data for word in row) + 8  # padding
         for row in data:
             print("".join(word.ljust(col_width) for word in row))
-        '''print("NAME:", self._name, "\t\t\tBIRTHDAY:", self._bday, "\tSEX:", self._sex)
-        print("ADDRESS:", self._addr, "\t\t\tHOME PHONE:", self._homePhone)
-        print("CITY:", self._city, "\tSTATE:", self._state, "\tZIP CODE:", self._zip)
-        print("OCCUPATION:", self._occ, "\t\t\tWORK PHONE:", self._workPhone)'''
+        print("-----------------------------------------------------------")
+
+
+class EmergencyContact:
+    def __init__(self, name, homePhone, workPhone):
+        self._name = name
+        self._homePhone = homePhone
+        self._workPhone = workPhone
+
+    def display_Emergency_Contact(self):
+        data = [["NAME: "+self._name, "HOME PHONE: "+self._homePhone],
+                ["","WORK PHONE: "+self._workPhone]]
+        col_width = max(len(word) for row in data for word in row) + 8  # padding
+        for row in data:
+            print("".join(word.ljust(col_width) for word in row))
+        print("-----------------------------------------------------------")
+
 
 
 obj = Enrollee('Andrew Chiang', '1/11/2001', 'M','3 Fords Run',
     '7818129168','Stoughton','MA','02072','Intern','7818129168')
-
-obj.displayEnrollee()
+obj.display_Enrollee()
+obj = EmergencyContact('Tracey Chiang', '7813416897','7819298481')
+print("IN CASE OF AN EMERGENCY, PLEASE CONTACT:")
+obj.display_Emergency_Contact()
